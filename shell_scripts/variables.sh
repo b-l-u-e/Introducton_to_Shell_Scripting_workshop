2bc33d79f038d1b6347629ec5da7e92bf60a202f
b.l.u.e <winnie.gitau282@gmail.com>
2023-05-20 11:13:39 +0300

    Updated variables shell script file

--- shell_scripts/variables.sh
+++ shell_scripts/variables.sh
@@ -1,2 +1,10 @@
 #! /usr/bin/bash
 
+# System defined variables
+echo "User: $USER"
+echo "Working directory: $PWD"
+
+# User defined variables
+FIRST_NAME="Winnie"
+LAST_NAME="Gitau"
+echo "My name is $FIRST_NAME $LAST_NAME"
